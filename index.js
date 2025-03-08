0ecca93cea7a2d6e29d8372c6718e6ba2c68a3f7
Divya-Darshan <parukrish.darshan20@gamil.com>
2025-03-08 11:57:19 +0530

    update with new content!

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 let btn = document.getElementById('no');
 let img = document.getElementById('img')
 // Ensure the button has absolute positioning
-
+let para = document.getElementById('p')
 
 
 function no() {
     btn.style.position = "absolute";
     img.src = "pinch.gif"
+    para.textContent = "You are really mean.😭"
     let screenWidth = window.innerWidth;
     let screenHeight = window.innerHeight;
     let btnWidth = btn.offsetWidth;
@@ -17,4 +18,10 @@ function no() {
 
     btn.style.left = randomX + "px";
     btn.style.top = randomY + "px";
+}
+
+function yes(){
+    para.textContent = "I knew it!!"
+    img.src = "reszz.gif"
+
 }
